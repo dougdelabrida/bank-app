02e62afac875fc9ba7b3180707b2d368bbe0b2a0
Douglas Delabrida <dougdelabrida@gmail.com>
2018-03-24 09:40:08 -0300

    set webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    filename: 'js/app.js',
+    path: path.resolve('dist')
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ["react", "stage-2"]
+        }        
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/www/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+}
