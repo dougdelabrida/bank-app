66efe131d3b7bd619567149628ffc9fd8817220c
Douglas Delabrida <dougdelabrida@gmail.com>
2018-03-24 14:46:05 -0300

    created simple header using styled-components

---
+++ src/client/components/Header.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default styled.header`
+  background: ${props => props.theme.blueGradient};
+  height: 64px;
+  width: 100%;
+`;
--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import { injectGlobal, ThemeProvider } from 'styled-components';
+import Header from './components/Header';
+
+injectGlobal`
+  html, body {
+    height: 100%;
+    margin: 0;
+  }
+`
+
+const mainTheme = {
+  blueGradient: 'linear-gradient(to right, #000428, #004e92)'
+}
+
 class TestComponent extends Component {
   state = { message: "Test 2" }
   
   render() {
     return (
-      <span>{this.state.message}</span>
+      <ThemeProvider theme={mainTheme}>
+        <Header>
+          Teste
+        </Header>
+      </ThemeProvider>
     )
   }
 }
