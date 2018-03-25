dfde4e17aaeb80d6a73e0f56f8780fbdf8ac84c0
Douglas Delabrida <dougdelabrida@gmail.com>
2018-03-25 17:32:27 -0300

    added cors to back-end

--- src/server/index.js
+++ src/server/index.js
@@ -11,6 +11,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('dist'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const router = express.Router();
 
 // Base Response
