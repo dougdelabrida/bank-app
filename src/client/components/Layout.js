8e35059631bbad8730bd1fcc894ef04169720504
Douglas Delabrida <dougdelabrida@gmail.com>
2018-03-25 17:31:05 -0300

    set main as container

--- src/client/components/Layout.js
+++ src/client/components/Layout.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Header from './Header';
+
+const Main = styled.main`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 16px;
+  width: 100%;
+`;
 
 const Layout = ({children}) => (
   <div>
     <Header />
-    <main>
+    <Main>
       {children}
-    </main>
+    </Main>
   </div>
 )
 
